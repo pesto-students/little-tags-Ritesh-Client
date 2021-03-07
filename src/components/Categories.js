5abd9eefd8415561c39f472d5d498ae98b2d3e36
rio14 <riteshsinha14.rs@gmail.com>
2021-03-08 01:01:51 +0530

    add functionality to productlist page

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,24 +5,28 @@ function Categories() {
   const categories = [
     {
       id: 1,
+      title: "men clothing",
       catName: "menClothing",
       catImg:
         "https://images.unsplash.com/photo-1446214814726-e6074845b4ce?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80",
     },
     {
       id: 2,
+      title: "women clothing",
       catName: "womenClothing",
       catImg:
         "https://images.unsplash.com/photo-1571908598047-9b0049221e4e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8bGFkaWVzJTIwd2VkZGluZyUyMGluZGlhfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80",
     },
     {
       id: 3,
-      catName: "jewelry",
+      title: "jewelery",
+      catName: "jewelery",
       catImg:
         "https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGpld2Vscnl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80",
     },
     {
       id: 4,
+      title: "electronics",
       catName: "electronics",
       catImg:
         "https://images.unsplash.com/photo-1598832517527-7e5b15ee1b2d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjd8fGVsZWN0cm9uaWN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=376&q=80",
@@ -37,7 +41,12 @@ function Categories() {
         {categories.map((cat, index) => (
           <Link
             key={index}
-            to="/productList"
+            to={{
+              pathname: "/productList",
+              search: `${cat.catName}`,
+              title: `${cat.title}`,
+            }}
+            params={{ testvalue: "hello" }}
             className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
           >
             <div
--- src/lang.js
+++ src/lang.js
@@ -8,7 +8,7 @@ const langs = {
     menClothing: "Men Clothing",
     womenClothing: "Women Clothing",
     electronics: "Electronics",
-    jewelry: "Jewelry",
+    jewelery: "Jewelery",
     copyright: "© Copyright 2021. All Rights Reserved.",
     searchTitle: "Search",
     //
@@ -36,7 +36,7 @@ const langs = {
     menClothing: "Ropa de hombre",
     womenClothing: "Ropa de mujeres",
     electronics: "Electrónica",
-    jewelry: "Joyas",
+    jewelery: "Joyas",
     copyright: "© Derechos de autor 2021. Todos los derechos reservados.",
     searchTitle: "buscar",
     manageOrder: "administrar pedidos",
