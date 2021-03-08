7bcce5ab3ec75e04299a9e4c22b0379e1eb55f05
Manmeet2018 <manietdavv@gmail.com>
2021-03-08 19:41:02 +0530

    feat: Add ProductPage functionality

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,166 +1,170 @@
 import React from "react";
 import { Link } from "react-router-dom";
-function ProductDetail() {
+import { withRouter } from "react-router";
+import Loading from "./Loading";
+function ProductDetail(props) {
+  const { data } = props.location;
+  const [item, setItem] = React.useState([]);
+  const [load, setLoad] = React.useState(true);
+  const [title, setTitle] = React.useState("");
+  React.useEffect(() => {
+    if (!data) {
+      setLoad(false);
+      props.history.goBack();
+    } else {
+      setItem(data.item);
+      setLoad(false);
+      setTitle(props.location.category.title);
+    }
+  }, [data, props.history, props.location]);
+  if (load) {
+    return <Loading />;
+  }
   return (
-    <div className="text-gray-700 body-font bg-white">
-      <div className="px-5 py-5 mx-auto">
-        <div className="lg:w-4/5 mx-auto flex flex-wrap">
+    <div className='text-gray-700 body-font bg-white'>
+      <div className='px-5 py-5 mx-auto'>
+        <div className='lg:w-4/5 mx-auto flex flex-wrap'>
           <img
-            alt="ecommerce"
-            className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
-            src="https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80"
+            alt='ecommerce'
+            className='lg:w-1/2 w-full object-cover object-center rounded border border-gray-200'
+            src={item.image}
           />
-          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-              The Catcher in the Rye
+          <div className='lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0'>
+            <h1 className='text-gray-900 text-3xl title-font font-medium mb-1'>
+              {item.title}
             </h1>
-            <span className="title-font font-medium text-2xl text-gray-900">
-              $58.00
+            <span className='title-font font-medium text-2xl text-gray-900'>
+              ${item.price}
             </span>
-            <div className="flex mb-4">
-              <span className="flex items-center">
+            <div className='flex mb-4'>
+              <span className='flex items-center'>
                 <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  fill='currentColor'
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  className='w-4 h-4 text-red-500'
+                  viewBox='0 0 24 24'>
+                  <path d='M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z'></path>
                 </svg>
                 <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  fill='currentColor'
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  className='w-4 h-4 text-red-500'
+                  viewBox='0 0 24 24'>
+                  <path d='M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z'></path>
                 </svg>
                 <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  fill='currentColor'
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  className='w-4 h-4 text-red-500'
+                  viewBox='0 0 24 24'>
+                  <path d='M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z'></path>
                 </svg>
                 <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  fill='currentColor'
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  className='w-4 h-4 text-red-500'
+                  viewBox='0 0 24 24'>
+                  <path d='M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z'></path>
                 </svg>
                 <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                  fill='none'
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  className='w-4 h-4 text-red-500'
+                  viewBox='0 0 24 24'>
+                  <path d='M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z'></path>
                 </svg>
-                <span className="text-gray-600 ml-3">4 Reviews</span>
+                <span className='text-gray-600 ml-3'>4 Reviews</span>
               </span>
             </div>
-            <p className="leading-relaxed">
-              Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-              sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-              juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-              seitan poutine tumeric. Gastropub blue bottle austin listicle
-              pour-over, neutra jean shorts keytar banjo tattooed umami
-              cardigan.
-            </p>
-            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
-              <div className="flex items-center mr-4">
-                <span className="mr-3">Size</span>
-                <div className="relative">
-                  <select className="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-                    <option>SM</option>
-                    <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
-                  </select>
-                  <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-4 h-4"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M6 9l6 6 6-6"></path>
-                    </svg>
-                  </span>
+            <p className='leading-relaxed'>{item.description}</p>
+            <div className='flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5'>
+              {title.includes("clothing") ? (
+                <div className='flex items-center mr-4'>
+                  <span className='mr-3'>Size</span>
+                  <div className='relative'>
+                    <select className='rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10'>
+                      <option>SM</option>
+                      <option>M</option>
+                      <option>L</option>
+                      <option>XL</option>
+                    </select>
+                    <span className='absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center'>
+                      <svg
+                        fill='none'
+                        stroke='currentColor'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        className='w-4 h-4'
+                        viewBox='0 0 24 24'>
+                        <path d='M6 9l6 6 6-6'></path>
+                      </svg>
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <div className="flex items-center">
-                <span className="mr-3">Quantity</span>
-                <div className="relative">
+              ) : undefined}
+              <div className='flex items-center'>
+                <span className='mr-3'>Quantity</span>
+                <div className='relative'>
                   <input
-                    className="w-auto px-3 py-2  border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
-                    placeholder="1"
-                    type="number"
-                    min="1"
-                    max="30"
-                    step="1"
+                    className='w-auto px-3 py-2  border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors'
+                    placeholder='1'
+                    type='number'
+                    min='1'
+                    max='30'
+                    step='1'
                   />
                 </div>
               </div>
             </div>
-            <div className="flex flex-row items-center  sm:text-md text-xs">
+            <div className='flex flex-row items-center  sm:text-md text-xs'>
               <Link
-                to="/"
-                className="flex items-center sm:mr-4 mr-2 text-white bg-black border-0 py-2 px-6 "
-              >
+                to='/'
+                className='flex items-center sm:mr-4 mr-2 text-white bg-black border-0 py-2 px-6 '>
                 <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
+                  fill='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  className='w-5 h-5'
+                  viewBox='0 0 24 24'>
                   <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z'
                   />
                 </svg>
-                <span className="pl-2 py-2">Add to cart</span>
+                <span className='pl-2 py-2'>Add to cart</span>
               </Link>
               <Link
-                to="/"
-                className="flex items-center text-gray border-2 py-2 px-6 rounded hover:bg-blue-700 hover:text-white"
-              >
+                to='/'
+                className='flex items-center text-gray border-2 py-2 px-6 rounded hover:bg-blue-700 hover:text-white'>
                 <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                  fill='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  className='w-5 h-5'
+                  viewBox='0 0 24 24'>
+                  <path d='M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z'></path>
                 </svg>
-                <span className="pl-2 py-2">Move to wishlist</span>
+                <span className='pl-2 py-2'>Move to wishlist</span>
               </Link>
             </div>
           </div>
@@ -170,4 +174,4 @@ function ProductDetail() {
   );
 }
 
-export default ProductDetail;
+export default withRouter(ProductDetail);
