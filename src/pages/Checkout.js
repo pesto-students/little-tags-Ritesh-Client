e7a142a4a80f9dab668d849b0347a69b43e12670
rio14 <riteshsinha14.rs@gmail.com>
2021-03-12 13:54:10 +0530

    Add mobile ui

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -141,10 +141,10 @@ function Checkout() {
             </div>
           </div>
         </div>
-        <div className="flex flex-row justify-between mt-12 i ">
-          <div className="w-3/4 shadow-md rounded-b-md mr-2">
+        <div className="flex sm:flex-row flex-col mt-12">
+          <div className="w-3/4 sm:block hidden shadow-md rounded-b-md mr-2">
             {itemList.map((item) => (
-              <div
+              <Link
                 key={item.id}
                 className="flex flex-row justify-between items-center border border-gray-300 rounded-lg shadow-inner mb-4 p-4"
               >
@@ -155,43 +155,221 @@ function Checkout() {
                     alt="Thumbnail"
                   />
                 </div>
-                <div className="w-1/2 flex flex-col justify-start">
+                <div className="w-1/2 flex flex-col justify-start space-y-4">
                   <span className="text-md font-medium">{item.title}</span>
-                  <div className="my-2">
-                    <span className="text-sm font-normal pr-4">Quantity</span>
-                    <input
-                      className="px-2 py-2 border-2 border-blue-200 rounded-lg focus:outline-none focus:border-blue-700 transition-colors"
-                      placeholder="1"
-                      type="number"
-                      min="1"
-                      max="30"
-                      step="1"
-                    />
+                  <div className="flex mt-6 items-center text-sm font-normal">
+                    {"men clothing".includes("clothing") ? (
+                      <div className="flex items-center mr-4">
+                        <span className="mr-3">Size</span>
+                        <div className="relative bg-transparent">
+                          <select className="rounded border bg-transparent appearance-none border-blue-200 py-2 focus:outline-none focus:border-red-500 pl-3 pr-10">
+                            <option>SM</option>
+                            <option>M</option>
+                            <option>L</option>
+                            <option>XL</option>
+                          </select>
+                          <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                            <svg
+                              fill="none"
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              className="w-4 h-4"
+                              viewBox="0 0 24 24"
+                            >
+                              <path d="M6 9l6 6 6-6"></path>
+                            </svg>
+                          </span>
+                        </div>
+                      </div>
+                    ) : undefined}
+                    <div className="flex items-center">
+                      <span className="mr-3">Quantity</span>
+                      <div className="relative">
+                        <input
+                          className="w-auto px-3 py-2  border-2 border-blue-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
+                          placeholder="1"
+                          type="number"
+                          min="1"
+                          max="30"
+                          step="1"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="text-left text-sm font-medium tracking-wide">
+                    <span className="font-semibold text-gray-600">
+                      Price :
+                      <span className="text-gray-500 mt-2">
+                        ${item.price}
+                        <span className="mx-2 text-xs line-through">
+                          ${(item.price + (item.price * 35) / 100).toFixed(2)}
+                        </span>
+                        <span className="text-blue-700">(35% off)</span>
+                      </span>
+                    </span>
                   </div>
                 </div>
-                <div className="w-1/3 flex flex-col justify-around items-center">
-                  <span className="text-lg font-normal">${item.price}</span>
-                  <Link
-                    to="/"
-                    className="mt-2 flex items-center text-blue-700 border-blue-200  border-2 p-2 rounded-lg hover:bg-blue-700 hover:text-white"
-                  >
-                    <svg
-                      fill="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
+                <div className="w-1/3 flex flex-col justify-center items-center text-sm font-light">
+                  <div className="w-hull">
+                    <Link
+                      to="/"
+                      className="my-2 flex items-center text-blue-700 border-blue-200  border-2 p-2 rounded-lg hover:bg-blue-700 hover:text-white"
                     >
-                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                    </svg>
-                    <span className="pl-2">Move to wishlist</span>
-                  </Link>
+                      <svg
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                      </svg>
+                      <span className="pl-2">Move to wishlist</span>
+                    </Link>
+                    <Link
+                      to="/"
+                      className="flex items-center text-blue-700 border-blue-200  border-2 p-2 rounded-lg hover:bg-blue-700 hover:text-white"
+                    >
+                      <svg
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                        />
+                      </svg>
+                      <span className="pl-2">Remove</span>
+                    </Link>
+                  </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
-          <div className="w-1/4 flex flex-col justify-between shadow-md rounded-b-md ">
+          {/* mobile view for cart item */}
+          <div className="sm:hidden block">
+            <div className="shadow-md rounded-b-md">
+              {itemList.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex flex-col justify-between items-center border border-gray-300 rounded-lg shadow-inner mb-4 p-4"
+                >
+                  <div className="flex flex-row justify-around">
+                    <div className="w-1/3 flex justify-center items-center mr-2">
+                      <img
+                        src={item.image}
+                        className="bg-gray-300"
+                        alt="Thumbnail"
+                      />
+                    </div>
+                    <div className="w-1/2 flex flex-col justify-start">
+                      <span className="text-md font-medium">{item.title}</span>
+                      <div className="flex flex-col justify-between mt-2 text-xs font-light">
+                        {"men clothing".includes("clothing") ? (
+                          <div className="flex items-center justify-between">
+                            <span className="mr-2">Size</span>
+                            <div className="relative">
+                              <select className="rounded-lg border-2 appearance-none border-blue-100 py-2 focus:outline-none bg-transparent pl-3 pr-10">
+                                <option>SM</option>
+                                <option>M</option>
+                                <option>L</option>
+                                <option>XL</option>
+                              </select>
+                              <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                                <svg
+                                  fill="none"
+                                  stroke="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  className="w-4 h-4"
+                                  viewBox="0 0 24 24"
+                                >
+                                  <path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                              </span>
+                            </div>
+                          </div>
+                        ) : undefined}
+                        <div className="flex items-center justify-between mt-2 text-xs font-light">
+                          <span className="pr-2">Quantity</span>
+                          <input
+                            className="py-2 text-black px-2 border-2 border-blue-100 rounded-lg focus:outline-none focus:border-blue-700 transition-colors"
+                            placeholder="1"
+                            type="number"
+                            min="1"
+                            max="30"
+                            step="1"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="text-sm font-medium tracking-wide my-1">
+                    <span className="font-semibold text-gray-600">
+                      Price :
+                      <span className="text-gray-500 mt-2">
+                        ${item.price}
+                        <span className="mx-2 text-xs line-through">
+                          ${(item.price + (item.price * 35) / 100).toFixed(2)}
+                        </span>
+                        <span className="text-blue-700">(35% off)</span>
+                      </span>
+                    </span>
+                  </div>
+                  <div className="mt-2 flex flex-row justify-between space-x-1 items-center text-sm font-light">
+                    <Link
+                      to="/"
+                      className="flex items-center text-blue-700 border-blue-200  border-2 p-2 rounded-lg hover:bg-blue-700 hover:text-white"
+                    >
+                      <svg
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                        />
+                      </svg>
+                      <span className="pl-2">Remove</span>
+                    </Link>
+                    <Link
+                      to="/"
+                      className="flex items-center text-blue-700 border-blue-200  border-2 p-2 rounded-lg hover:bg-blue-700 hover:text-white"
+                    >
+                      <svg
+                        fill="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                      </svg>
+                      <span className="pl-2">Move to wishlist</span>
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="sm:w-1/4 flex flex-col space-y-2 shadow-md rounded-b-md ">
             <div className="py-2 border-b-2 rounded-b-lg  border-gray-200 px-2 shadow-inner">
               <span className="text-sm uppercase font-medium tracking-normal">
                 Coupons
@@ -211,9 +389,12 @@ function Checkout() {
                     d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
                   />
                 </svg>
-                <span className="text-sm capitalize  font-medium tracking-normal">
-                  Apply coupon
-                </span>
+                <input
+                  type="text"
+                  placeholder="Apply Coupon"
+                  className="w-4/6 mx-1 text-gray-700 border-b border-blue-200 focus:outline-none focus:ring focus:border-blue-300"
+                />
+
                 <button className=" text-blue-700 border-blue-200  border-2 p-2 rounded-lg hover:bg-blue-700 hover:text-white">
                   Apply
                 </button>
@@ -284,6 +465,31 @@ function Checkout() {
                   Place order
                 </button>
               </div>
+            </div>
+          </div>
+          <div className="sm:hidden block">
+            <div className="fixed inset-x-0 text-blue-700 bottom-0 flex justify-around items-center bg-white border-t border-blue-200 rounded-t-sm p-1 shadow-sm">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 10l7-7m0 0l7 7m-7-7v18"
+                />
+              </svg>
+              <span className="text-md font-medium text-center">$571.34</span>
+              <button
+                onClick={handleStepper}
+                className="w-4/6 border-blue-200  border-2 p-2 rounded-lg bg-blue-700 text-white"
+              >
+                Place order
+              </button>
             </div>
           </div>
         </div>
