f1e4831cb5987e235bf2b515438e6123c0c0e329
rio14 <riteshsinha14.rs@gmail.com>
2021-03-14 14:25:51 +0530

    -update: Search auto complete

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,54 +1,33 @@
 import React from "react";
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import { FormattedMessage } from "react-intl";
+import { useHistory } from "react-router-dom";
+import data from "../locatlData/data";
 function Search() {
-  const items = [
-    {
-      id: 0,
-      name: "MEN CLOTHING",
-    },
-    {
-      id: 1,
-      name: "WOMEN CLOTHING",
-    },
-    {
-      id: 2,
-      name: "JEWELRY",
-    },
-    {
-      id: 3,
-      name: "ELECTRONICS",
-    },
-  ];
-
-  const handleOnSearch = (string, results) => {
-    // onSearch will have as the first callback parameter
-    // the string searched and for the second the results.
-    console.log("search", string, results);
-  };
-
-  const handleOnSelect = (item) => {
-    // the item selected
-    console.log(item);
-  };
-
-  const handleOnFocus = () => {
-    console.log("Focused");
+  const history = useHistory();
+  const handleOnSelect = item => {
+    history.push({
+      pathname: "/productDetail",
+      search: `${item.title}`,
+      data: { item },
+      category: { item },
+    });
   };
 
   return (
     <FormattedMessage id="searchTitle" defaultMessage="search">
-      {(placeholder) => {
+      {placeholder => {
         return (
           <ReactSearchAutocomplete
-            items={items}
-            onSearch={handleOnSearch}
+            items={data}
+            fuseOptions={{ keys: ["title", "category"] }}
+            resultStringKeyName="title"
             onSelect={handleOnSelect}
-            onFocus={handleOnFocus}
             placeholder={placeholder[0]}
             styling={{
               borderRadius: "7px",
               color: "gray",
+              fontSize: "14px",
             }}
           />
         );
