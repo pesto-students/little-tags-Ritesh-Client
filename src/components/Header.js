88bb08f55f026b2a3ef003d514040cb02b852c0c
rio14 <riteshsinha14.rs@gmail.com>
2021-03-15 12:25:57 +0530

    add firebase setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import Login from "./Login";
 import Navbar from "./Navbar";
-function Header({ props, changeLanguage }) {
+function Header({ changeLanguage }) {
   const [showModal, setShowModal] = React.useState(false);
 
   return (
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+import firebase from "./firebaseConfig";
+
+export const facebookProvider = new firebase.auth.FacebookAuthProvider();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
