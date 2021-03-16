52de97c717c96ec6f585a5bd4bb0c6d31105872a
Manmeet2018 <manietdavv@gmail.com>
2021-03-17 00:41:40 +0530

    fix: DataUpdated

--- src/localData/data.js
+++ src/localData/data.js
@@ -36,7 +36,8 @@ const data = [
     description:
       "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
     category: "men clothing",
-    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71NcFEriB3L._UX385_.jpg",
   },
   {
     id: 5,
@@ -81,36 +82,40 @@ const data = [
       "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
     price: 695,
     description:
-      "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+      "Balika Vadhu Jewellery Designer Jwellery Set for Women & Girls (Mehandi/Haldi /Bridal/Baby Shower/Marriage)",
     category: "jewelry",
-    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81sseeRgRkL._UX679_.jpg",
   },
   {
     id: 10,
-    title: "Solid Gold Petite Micropave ",
+    title: "Sukkhi Glamorous LCT Gold Plated Wedding Jewellery",
     price: 168,
     description:
-      "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+      "Sukkhi Glamorous LCT Gold Plated Wedding Jewellery Pearl Choker Necklace Set Combo For Women (CB73381)",
     category: "jewelry",
-    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/912iX-Sve0L._UY575_.jpg",
   },
   {
     id: 11,
     title: "White Gold Plated Princess",
     price: 9.99,
     description:
-      "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+      "Sukkhi Adorable Gold Plated Pearl Choker Necklace Set for Women",
     category: "jewelry",
-    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81UaMFQR6kL._UY575_.jpg",
   },
   {
     id: 12,
     title: "Pierced Owl Rose Gold Plated Stainless Steel Double",
     price: 10.99,
-    description:
-      "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
     category: "jewelry",
-    image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+    description:
+      "Sapphire Blue Silver 18K White Gold Plated Austrian Crystal Kate Middleton's Princess Diana Inspired Stud Earrings Romantic Love Party Wear Jewellery for Women by Caratcube (CTC - 107)",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51p86z3P2GL._UY575_.jpg",
   },
   {
     id: 13,
